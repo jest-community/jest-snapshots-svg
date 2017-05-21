e367c687b51b3c5dd14318d336f6418b22e25d2c
Orta Therox <orta.therox@gmail.com>
2017-05-21 20:48:37 +0100

    Handle aligning items correctly.

--- src/_tests/example_layouts/_align-items.test.tsx
+++ src/_tests/example_layouts/_align-items.test.tsx
@@ -8,16 +8,16 @@ import * as renderer from "react-test-renderer"
 
 it("Renders three vertically/horizontally centeredblocks", () => {
   const jsx =
-      <View style={{
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-        <View style={{width: 50, height: 50, backgroundColor: "powderblue"}} />
-        <View style={{width: 50, height: 50, backgroundColor: "skyblue"}} />
-        <View style={{width: 50, height: 50, backgroundColor: "steelblue"}} />
-      </View>
+    <View style={{
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <View style={{ width: 50, height: 50, backgroundColor: "powderblue" }} />
+      <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }} />
+      <View style={{ width: 50, height: 50, backgroundColor: "steelblue" }} />
+    </View>
 
   const component = renderer.create(jsx).toJSON()
   expect(component).toMatchSVGSnapshot(320, 480)
