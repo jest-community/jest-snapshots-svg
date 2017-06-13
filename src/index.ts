1e73ed5f8eada2459204bdf1041fd72d158de2c5
Orta Therox <orta.therox@gmail.com>
2017-06-13 12:02:47 +0100

    Use the test name inside the file

--- src/index.ts
+++ src/index.ts
@@ -42,12 +42,16 @@ expect.extend({
         if (!root.props) { return fail("A Component without props was passed to toMatchSVGSnapshot") }
         if (!root.type) { return fail("A Component without a type was passed to toMatchSVGSnapshot") }
 
-        // This isn't in the d.ts
-        const currentTest = (expect as any).getState().testPath
+        // getState isn't in the d.ts for Jest, this is ok though.
+        const state = (expect as any).getState()
+        const currentTest = state.testPath as string
+        const currentTestName = state.currentTestName as string
+
+        const testFile = currentTestName.replace(/\s+/g, "-").replace(/\//g, "-").toLowerCase()
 
         //  Figure out the paths
         const snapshotsDir = path.join(currentTest, "..", "__snapshots__")
-        const expectedSnapshot = path.join(snapshotsDir, path.basename(currentTest) + ".svg")
+        const expectedSnapshot = path.join(snapshotsDir, path.basename(currentTest) + "-" + testFile + ".svg")
 
         // Make our folder if it's needed
         if (!fs.existsSync(snapshotsDir)) { fs.mkdirSync(snapshotsDir) }
@@ -69,10 +73,9 @@ expect.extend({
 
         const svgText = treeToSVG(renderedComponentRoot, settings)
 
-        // TODO: Dertermine if Jest is in `-u`?
-        //
-        // const GLOBAL_STATE = Symbol.for("$$jest-matchers-object")
-        // console.log(global[GLOBAL_STATE].state)
+        // TODO: Determine if Jest is in `-u`?
+        // can be done via the private API
+        // state.snapshotState._updateSnapshot === "all"
 
         // Are we in write mode?
         if (!fs.existsSync(expectedSnapshot)) {
